4ff52844ff045436479d1ea307bb37d9e0f1d24d
Nathaniel Mason <nmason0204@gmail.com>
2022-02-19 00:28:02 -0800

    Skeleton enemy follows player now

--- Assets/EnemyFollow.cs
+++ Assets/EnemyFollow.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour
 {
-    private GameObject Player;
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = GameObject.Find("Player");
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+        //check distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distToPlayer < agroRange)
+        {
+            //chase the player
+            ChasePlayer();
+        }
+        else
+        {
+            //stop chasing player
+            StopChasingPlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //enemy is to the left of player so move right
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            transform.localScale = new Vector2(2, 2);
+        }
+        else
+        {
+            //enemy is to the right of player so move left
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            transform.localScale = new Vector2(-2, 2);
+        }
+    }
+    private void StopChasingPlayer()
+    {
+        rb2d.velocity = Vector2.zero;
     }
 }
